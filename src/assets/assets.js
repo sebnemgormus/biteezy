b212c07ffaa434de5d32cba5eb55554b579b7f21
sebnem <sebnemgormus@gmail.com>
2025-04-22 17:35:36 +0300

    Implement Context API and create FoodDisplay & FoodItem components

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -12,12 +12,31 @@ import menu_7 from './menu_7.png'
 import menu_8 from './menu_8.png'
 
 
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+
+
+import rating_starts from './rating_starts.png'
+import add_icon_white from './add_icon_white.png'
+import remove_icon_red from './remove_icon_red.png'
+import add_icon_green from './add_icon_green.png'
+
+
 export const assets = {
     logo,
     basket_icon,
     header_img,
     search_icon,
-    
+    rating_starts,
+    add_icon_white,
+    remove_icon_red,
+    add_icon_green
 }
 
 export const menu_list = [
@@ -26,20 +45,16 @@ export const menu_list = [
         menu_image: menu_1
     },
     {
-        menu_name: "Rolls",
+        menu_name: "Wraps",
         menu_image: menu_2
     },
     {
-        menu_name: "Deserts",
+        menu_name: "Dessert",
         menu_image: menu_3
     },
     {
         menu_name: "Sandwich",
         menu_image: menu_4
-    },
-    {
-        menu_name: "Cake",
-        menu_image: menu_5
     },
     {
         menu_name: "Pure Veg",
@@ -52,5 +67,66 @@ export const menu_list = [
     {
         menu_name: "Noodles",
         menu_image: menu_8
-    }]
+    }
+]
 
+export const food_list = [
+    {
+        _id: "1",
+        name: "Special Salad",
+        image: food_1,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Salad"
+    },
+    {
+        _id: "2",
+        name: "Veg salad",
+        image: food_2,
+        price: 18,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Salad"
+    }, {
+        _id: "3",
+        name: "Clover Salad",
+        image: food_3,
+        price: 16,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Salad"
+    }, {
+        _id: "4",
+        name: "Chicken Salad",
+        image: food_4,
+        price: 24,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Salad"
+    }, {
+        _id: "5",
+        name: "Lasagna Rolls",
+        image: food_5,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Rolls"
+    }, {
+        _id: "6",
+        name: "Peri Peri Rolls",
+        image: food_6,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Rolls"
+    }, {
+        _id: "7",
+        name: "Chicken Rolls",
+        image: food_7,
+        price: 20,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Rolls"
+    }, {
+        _id: "8",
+        name: "Veg Rolls",
+        image: food_8,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Rolls"
+    }
+]
