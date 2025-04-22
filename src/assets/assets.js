068164ffeba39964fe2a6903c19c9cdf8c715413
sebnem <sebnemgormus@gmail.com>
2025-04-22 13:02:21 +0300

    add Navbar component

---
+++ src/assets/assets.js
@@ -0,0 +1,12 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import search_icon from './search_icon.png'
+
+
+export const assets = {
+    logo,
+    basket_icon,
+    search_icon,
+  
+}
+
