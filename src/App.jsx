75fd24a8870c2a7018a979def722278ab9491673
sebnem <sebnemgormus@gmail.com>
2025-05-02 18:20:50 +0300

    fix: correct typo in URL path

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
+          <Route path="/card" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
         </Routes>
       </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = ({setShowLogin}) => {
         <div className='navbar-right'>
             <img src={assets.search_icon} alt="" />
             <div className='navbar-search-icon'>
-                <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
+                <Link to='/card'><img src={assets.basket_icon} alt="" /></Link>
                 <div className={getTotalCartAmount()===0?"":"dot"}></div>
             </div>
             <button onClick={()=> setShowLogin(true)}>Sign In</button>
